da304604322b16aef0fe096ab4e0c66ff7819219
Sarah Ronald <sareronald@hotmail.com>
2021-02-01 11:10:21 +1100

    refining landing page graphics

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,33 +1,47 @@
 import React, { Component } from "react";
 import "./style.css";
-import logo from "../../assets/images/ttt-logo.png";
+// import logo from "../../assets/images/ttt-logo.png";
+import { Link } from "react-router-dom";
 
 class Landing extends Component {
   render() {
     return (
       <div id="landingPage" className="container-fluid">
-        <div className="row justify-content-center">
-          <div className="col-sm-auto">
-            <img id="logo" src={logo} alt="logo" />
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-sm-6 landingPageTextDiv">
-            <div id="info" className="text-center">
-              <p>Everyone learns differently.</p>
-              <p>
-                TIC-TAC-TOE is a learning tool that allows teachers to create
-                interactive tic-tac-toe board(s) to add differentiation to their
-                classroom - at school, at home or anywhere in an online learning
-                environment that encourages success for all.
-              </p>
+        <div className="row">
+          <div className="col-md-6 center-cube">
+            <div id="container">
+              <div id="imasters-cube">
+                <div class="front"></div>
+                <div class="back"></div>
+                <div class="top"></div>
+                <div class="right"></div>
+                <div class="bottom"></div>
+                <div class="left"></div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row justify-content-center">
-          <button type="button" class="btn btn-primary shadow p-3 mb-5 rounded">
-            Get Started!
-          </button>
+
+          <div className="col-md-4 landingPageTextDiv text-center">
+            <div className="text-center">
+              <h1 className="infoTitle">TIC-TAC-TOE</h1>
+              <h2 className="infoByline">Everyone learns differently.</h2>
+              <p id="info">
+                TIC-TAC-TOE is a learning tool that allows teachers to create
+                interactive tic-tac-toe boards to add differentiation to their
+                classroom - at school, at home or anywhere in an online learning
+                environment that encourages success for all. To get started,
+                just click on a button below.
+              </p>
+            </div>
+            <div className="row justify-content-center">
+              <Link to={"/login"} type="button" className="logBtn">
+                LOGIN
+              </Link>
+              <Link type="button" className="logBtn" class="logBtn">
+                SIGNUP
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     );
