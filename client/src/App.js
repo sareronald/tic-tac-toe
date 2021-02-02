dfe827e39e72f009887e2dcbe3973ef1364f669f
Sarah Ronald <sareronald@hotmail.com>
2021-02-02 21:57:30 +1100

    updating app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing/Landing";
 // import Navbar from "./components/Navbar/Navbar";
-import Tictactoe from "./pages/Tictactoe";
+// import Tictactoe from "./pages/Tictactoe/Tictactoe";
 // import Signup from "./components/Signup";
 // import Login from "./components/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container-fluid pl-0 pr-0 m-0">
-          <Route exact path="/" component={Landing} />
-          <div className="container-fliud m-0 p-0">
-            {/* <Route exact path="/signup" component={Signup} /> */}
-            {/* <Route exact path="/login" component={Login} /> */}
-            <Route exact path="/tictactoe" component={Tictactoe} />
-          </div>
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="container-fluid pl-0 pr-0 m-0">
+        <Route exact path="/" component={Landing} />
+        <div className="container-fliud m-0 p-0">
+          {/* <Route exact path="/signup" component={Signup} /> */}
+          {/* <Route exact path="/login" component={Login} /> */}
+          {/* <Route exact path="/tictactoe" component={Tictactoe} /> */}
         </div>
-      </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
 }
 
 export default App;
