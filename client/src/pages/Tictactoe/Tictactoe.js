abc0dc887f2e57b9e87c9faece2bcebd38e4c757
Sarah Ronald <sareronald@hotmail.com>
2021-02-11 19:48:06 +1100

    renoving Navbar

--- client/src/pages/Tictactoe/Tictactoe.js
+++ client/src/pages/Tictactoe/Tictactoe.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import Navbar from "../../components/Navbar/Navbar";
 import TictactoeBoard from "../../components/TictactoeBoard/TictactoeBoard";
 import Instructions from "../../components/Instructions/Instructions";
 import "./style.css";
@@ -7,7 +6,6 @@ import "./style.css";
 function Tictactoe() {
   return (
     <div className="container-fluid p0 m0">
-      {/* <Navbar /> */}
       <div className="row">
         <div className="col-sm-6">
           <TictactoeBoard />
