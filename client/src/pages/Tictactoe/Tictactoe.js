d671ca1aa0d504f1345a2c7966c3b5aa8d349772
Sarah Ronald <sareronald@hotmail.com>
2021-02-14 09:48:39 +1100

    adding a title to activities page

--- client/src/pages/Tictactoe/Tictactoe.js
+++ client/src/pages/Tictactoe/Tictactoe.js
@@ -12,10 +12,13 @@ function Tictactoe() {
   return (
     <div className="container-fluid p0 m0">
       <div className="row">
-        <div className="col-sm-6">
+        <div className="col-sm-5" style={{ marginTop: "40px" }}>
           <TictactoeBoard />
         </div>
-        <div className="col-sm-6 instructions">
+        <div className="col-sm-7 instructions">
+          <h1 style={{ letterSpacing: "0.1em", marginLeft: "20px" }}>
+            Click on the squares to add Activity details
+          </h1>
           <Instructions />
         </div>
       </div>
